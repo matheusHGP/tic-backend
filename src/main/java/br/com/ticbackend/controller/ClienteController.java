601c9aee7c11e27002a4af29d554dc204d032d8c
thiago prado <thiagoprado549@gmail.com>
2021-06-20 23:54:06 -0300

    Criando endpoint de get with filter

--- src/main/java/br/com/ticbackend/controller/ClienteController.java
+++ src/main/java/br/com/ticbackend/controller/ClienteController.java
@@ -9,20 +9,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ticbackend.dto.ClienteDto;
-import br.com.ticbackend.dto.UsuarioDto;
 import br.com.ticbackend.model.Cliente;
 import br.com.ticbackend.repository.ClienteRepository;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/clientes")
 public class ClienteController {
 
@@ -32,16 +34,19 @@ public class ClienteController {
 	@Autowired
 	ModelMapper modelMapper;
 
-	@CrossOrigin("*")
-	@PostMapping("/")
+	@PostMapping()
 	public ResponseEntity<String> cliente(@RequestBody Cliente cliente) {
-		Cliente clienteModel = injecao.save(cliente);
+		injecao.save(cliente);
 		return new ResponseEntity<>("Cliente cadastrado", HttpStatus.OK);
 	}
 
-	@GetMapping("/")
-	public List<ClienteDto> getAll() {
-		List<Cliente> clientes = injecao.findAll();
+	@GetMapping()
+	public List<ClienteDto> getAll(@RequestParam(name = "nomeCliente", required = false) String nomeCliente,
+			@RequestParam(name = "cpfCliente", required = false) String cpfCliente) {
+
+		List<Cliente> clientes;
+
+		clientes = injecao.findAll(nomeCliente, cpfCliente);
 
 		List<ClienteDto> clientesDto = new ArrayList<ClienteDto>();
 
@@ -68,9 +73,15 @@ public class ClienteController {
 		return clienteDto;
 	}
 
-	private Cliente convertToEntity(ClienteDto clienteDto) {
-		Cliente cliente = modelMapper.map(clienteDto, Cliente.class);
-		return cliente;
+	// private Cliente convertToEntity(ClienteDto clienteDto) {
+	// Cliente cliente = modelMapper.map(clienteDto, Cliente.class);
+	// return cliente;
+	// }
+
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable Long id) {
+		injecao.deleteById(id);
+		return "Cliente removido";
 	}
 
 }
