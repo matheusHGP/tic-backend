5e91c7ee578c96120c7cbe998b88899749ab26fd
thiago prado <thiagoprado549@gmail.com>
2021-06-20 23:54:06 -0300

    Criando endpoint de get with filter

--- src/main/java/br/com/ticbackend/repository/ClienteRepository.java
+++ src/main/java/br/com/ticbackend/repository/ClienteRepository.java
@@ -1,9 +1,15 @@
 package br.com.ticbackend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.ticbackend.model.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	@Query("select c from Cliente c where (:nomeCliente is null or c.nomeCliente = :nomeCliente)"
+			+ "and (:cpfCliente is null or c.cpfCliente = :cpfCliente)")
+	public List<Cliente> findAll(String nomeCliente, String cpfCliente);
 
 }
